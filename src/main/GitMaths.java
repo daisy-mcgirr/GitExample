54eb56814714d892dd4e175c476498e6f789ba24
tst15 <tst15@LYTC-BUS15.ly.fastc.ie>
2018-08-29 14:26:59 +0100

    added multiply and main methods

--- src/main/GitMaths.java
+++ src/main/GitMaths.java
@@ -2,14 +2,23 @@ package main;
 
 public class GitMaths {
 
+	
 	public GitMaths() {
 
-		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("1+2 = " + add(1,2));
+		System.out.println("3+2 = " + multiply(3,2));
 		
 	}
 	
-	public int add(int a, int b) {
+	
+	public static int add(int a, int b) {
 		return a+b;
 	}
 
+	public static int multiply(int a, int b) {
+		return a*b;
+	}
 }
